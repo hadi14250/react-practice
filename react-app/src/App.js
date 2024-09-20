63feacd3894a04d01e9bf07775d4264cf97d8728
hadi14250 <hadihk2k@gmail.com>
2024-09-20 22:11:43 +0400

    rednered my own array by building a component called list that takes data, renderItem and renderEmpty and I defined the these while passing them as props in the App componenet

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,76 +1,42 @@
 import './App.css';
-import { useState, useEffect } from 'react';
 
-function GithubUser(
-  {
-    name, login, followers, following, avatar, location
-  }
-  )
-  {
-  return (
-     <div>
-      <h1>
-      {name}
-      </h1>
-      <p>User Name: {login}</p>
-      <p>Location: {location}</p>
-      <p>Followers: {followers}</p>
-      <p>following: {following}</p>
-      <p>profile picture:</p>
-      <img
-      src={avatar}
-      alt={name + " Profile Picture"}
-      height={150}
-      >
-      </img>
-     </div>
-  )
+const tahoe_peaks = [
+  { name: "Feel", elevaion: 10891},
+  { name: "Monument Peak", elevaion: 10067},
+  { name: "Pyramid", elevaion: 9983},
+  { name: "Tallac", elevaion: 9735},
+]
+
+function List({data, renderItem, renderEmpty}) {
+    return (!data.length ? (
+      renderEmpty
+      ) : (
+      <ul>{data.map( (item) => (
+      <li key={item.name}>
+        {renderItem(item)}
+      </li>
+      )
+    )}</ul>)
+    );
 }
 
 function App() {
 
-    const userName = "hadi14250"
-
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(
-    () => {
-      setLoading(true);
-
-      fetch(
-        `https://api.github.com/users/${userName}`
-        ).then((response) => {
-            response.json().then(setData).then(
-              () => {
-                setLoading(false);
-              }
-            ).catch (
-              setError // .catch function will automatically pass the error as a first argument to setError
-            );
+  return (
+      <List
+      data={tahoe_peaks}
+      renderEmpty={<p>This list is empty</p>}
+      renderItem={
+        (item) => {
+          return (
+          <>
+          {item.name} - {item.elevaion} ft.
+          </>
+          )
         }
-        ,
-        );
-  }, []
+      }
+      />
   )
-
-  if (loading) return(<h1>Loading...</h1>);
-  if (error)
-    return(<pre>{JSON.stringify(error)}</pre>)
-  if (!data)
-    return(<h1>failed to fetch {userName} info</h1>)
-
-      return (
-        <GithubUser
-        name={data.name}
-        location={data.location}
-        login={data.login}
-        followers={data.followers}
-        following={data.following}
-        avatar={data.avatar_url}
-        />
-      )
 }
 
 export default App;
