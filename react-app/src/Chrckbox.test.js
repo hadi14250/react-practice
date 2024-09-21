01d6b6e663673353967ca196a6ce52753cb49a1d
hadi14250 <hadihk2k@gmail.com>
2024-09-21 14:28:53 +0400

    added comment

--- react-app/src/Chrckbox.test.js
+++ react-app/src/Chrckbox.test.js
@@ -3,7 +3,22 @@ import { fireEvent, render } from "@testing-library/react";
 
 test("Selecting checkbox should change value to true", () => {
     const { getByLabelText } = render(<Checkbox />);
-    const checkbox = getByLabelText(/unchecked/);
+    const checkbox = getByLabelText(/unchecked/); // here it is not
+    // returning the text but it is returning the label that is associated with
+    // the text
     fireEvent.click(checkbox);
     expect(checkbox.checked).toBe(true);
 });
+
+
+/*
+
+In this context, checkbox is not just the return of a
+regex operation; it is the actual checkbox input element
+in the DOM. When you call getByLabelText, you're using
+the regex to locate the label, and then the Testing Library
+finds the associated input element (the checkbox).
+The fireEvent function operates on this element, simulating
+the interaction a user would have when they click the checkbox.
+
+*/
